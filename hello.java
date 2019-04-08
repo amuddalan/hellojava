f76df2fa38715e71d0b3003e18f1aeab2234be1b
Muddalapuram <anilkumar.muddalapuram@capgemini.com>
2019-04-08 14:52:28 +0530

    a

--- hello.java
+++ hello.java
@@ -1,12 +1,5 @@
-/* This is a simple Java program. 
-FileName : "HelloWorld.java". */
-class HelloWorld 
-{ 
-	// Your program begins with a call to main(). 
-	// Prints "Hello, World" to the terminal window. 
-	public static void main(String args[]) 
-	{ 
-		System.out.println("Hello, World"); 
-                System.out.println("Hello, ANIL"); 
-	} 
-} 
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
